482495d20c3755fe997ae3bfc9459416537021de
am903103 <am903103@au.edu.tw>
2018-08-15 12:00:05 +0800

    Add Component (Protocol)

---
+++ SwiftDecorator/MacBook.swift
@@ -0,0 +1,11 @@
+//
+// Created by Apple on 2018/8/15.
+// Copyright (c) 2018 Gamma. All rights reserved.
+//
+
+//Step 2. Add Component: MacBook Procotol
+
+protocol MacBook {
+    var cost: Double { get }
+    var description: String { get }
+}
