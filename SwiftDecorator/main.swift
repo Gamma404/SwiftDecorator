3f8cd7169e69d7b9d87e49d46dbd270574deaecf
am903103 <am903103@au.edu.tw>
2018-08-15 11:53:44 +0800

    Add Requirement in Main

---
+++ SwiftDecorator/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  SwiftDecorator
+//
+//  Created by Apple on 2018/8/15.
+//  Copyright © 2018年 Gamma. All rights reserved.
+//
+
+//Step 1: Requirement
+
+var macBookRegular: MacBook = MacBookRegulr()
+print("Cost: \(macBookRegular.cost) , Description: \(macBookRegular.drscription)")
+
+macBookRegular = ProcessorUpgrade(macBookRegular)
+print("Cost: \(macBookRegular.cost) , Description: \(macBookRegular.drscription)")
+
+macBookRegular = SSDUPgrade(macBookRegular)
+print("Cost: \(macBookRegular.cost) , Description: \(macBookRegular.drscription)")
+
+macBookRegular = TouchBarUpgrade(macBookRegular)
+print("Cost: \(macBookRegular.cost) , Description: \(macBookRegular.drscription)")
+
